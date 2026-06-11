90d6fc89f6880955fcd6715545e5ecc2a78b47e7
arybhatt4533 <bhattarya4533@gmai.com>
2026-06-11 21:39:10 +0530

    Access specifers

---
+++ access-specifers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class myClass {
+    private:
+     int x;
+    public:
+    int y;
+
+};
+ int main() {
+    myClass myObj;
+    
+    myObj.y=14;
+    cout << myObj.y<< endl;
+    
+
+ }
