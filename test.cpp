2eff95806a3e10b4de97af9b2c81f0f3aa358178
arybhatt4533 <bhattarya4533@gmai.com>
2026-06-24 20:58:31 +0530

    update

--- test.cpp
+++ test.cpp
@@ -31,8 +31,7 @@ void print(int n)
     // }
     // cout << endl;
 
-    // 
-    
+    //
 
     // for (int i = 1; i < n; i++)
     // {
@@ -44,15 +43,49 @@ void print(int n)
     // }
     // cout << endl;
 
-    for (int i = 1; i < n; i++)
+    // for (int i = 1; i < n; i++)
+    // {
+    //     for (int j = 1; j <=n-i-1; j++)
+    //     {
+    //         cout << j<<" ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // 1. Pehla Inner Loop: Spaces print karne ke liye (n - i - 1) baar
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         cout << " "; // Sirf ek single space
+    //     }
+        
+    //     // 2. Dusra Inner Loop: Stars print karne ke liye (i + 1) baar 
+    //     // Kyunki hum star ke sath space "* " chhap rahe hain, toh ye apne aap triangle bana dega!
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << "* "; 
+    //     }
+        
+    //     cout << endl; // Line change
+    // }
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <=n-i-1; j++)
+        // 1. Pehla Inner Loop: Spaces print karne ke liye (i) baar
+        for (int j = 0; j < i; j++)
         {
-            cout << j<<" ";
+            cout << " "; // Ek single space
         }
-        cout << endl;
+        
+        // 2. Dusra Inner Loop: Stars print karne ke liye (n - i) baar
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "* "; // Star ke sath space
+        }
+        
+        cout << endl; // Row khatam, line change
     }
-    cout << endl;
 };
 
 int main()
